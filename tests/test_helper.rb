760ab9505f7d3767f21839a7e545e34679cc6c33
Nick Klauer <klauer@gmail.com>
2011-08-23 22:19:02 -0500

    hack load path to be able to run single tests

--- tests/test_helper.rb
+++ tests/test_helper.rb
@@ -2,6 +2,7 @@ gem 'minitest'
 %w{ spec unit autorun pride }.each { |w|
   require "minitest/#{w}"
 }
+$:.unshift File.join(File.dirname(__FILE__), "../lib") # 'lib' directory
 require 'mantisrb'
 
 MANTISBT_URL = "http://www.mantisbt.org/demo"
