a9dc2f02a43b19dc7117589c96d94deb632f7738
Nick Klauer <klauer@gmail.com>
2011-08-24 22:47:46 -0500

    add project-type

---
+++ lib/mantisrb/projects/project.rb
@@ -0,0 +1,10 @@
+module Mantis
+
+  class Project
+    attr_accessor :name, :status, :enabled, :view_state, :inherit_global
+    attr_accessor :access_min, :file_path, :description, :subprojects
+    attr_reader :id
+
+  end
+end
+
