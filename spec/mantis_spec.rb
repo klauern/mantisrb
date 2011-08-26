59fe50a130a4304e99f43ab380d63b7d1c9e09cf
Nick Klauer <klauer@gmail.com>
2011-08-25 21:34:51 -0500

    abstract session creation to helper

--- spec/mantis_spec.rb
+++ spec/mantis_spec.rb
@@ -1,16 +1,9 @@
 require_relative 'spec_helper'
-      require 'base64'
 
 describe Mantis::Session do
 
   before do
-    if ENV['MANTIS_USER'] && ENV['MANTIS_PASS'] && ENV['MANTIS_URL']
-      @session = Mantis::Session.new ENV['MANTIS_URL'], ENV['MANTIS_USER'],
-        Base64.decode64(ENV['MANTIS_PASS'])
-    else
-      @session = Mantis::Session.new MANTIS_URL, MANTIS_USER, 
-        Base64.decode64(MANTIS_PASS)
-    end
+    @session = create_session
   end
 
   describe "Mantis" do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
+require 'base64'
 gem 'minitest'
 %w{ spec unit autorun pride }.each { |w|
   require "minitest/#{w}"
@@ -8,3 +9,14 @@ require 'mantisrb'
 MANTIS_URL = "http://www.plangineering.com/nek/mantis"
 MANTIS_USER = "admin"
 MANTIS_PASS = "RFBDSlBxYURURXpCMXpoeA=="
+
+def create_session
+  if ENV['MANTIS_USER'] && ENV['MANTIS_PASS'] && ENV['MANTIS_URL']
+    session = Mantis::Session.new ENV['MANTIS_URL'], ENV['MANTIS_USER'],
+      Base64.decode64(ENV['MANTIS_PASS'])
+  else
+    session = Mantis::Session.new MANTIS_URL, MANTIS_USER, 
+      Base64.decode64(MANTIS_PASS)
+  end
+  session
+end
