2fd2871de0192edc48e2380ac662519750e1d3f1
Nick Klauer <klauer@gmail.com>
2011-08-30 21:55:56 -0500

    Add ObjectRef to start abstracting XSD content into pieces

---
+++ lib/mantisrb/object_ref.rb
@@ -0,0 +1,42 @@
+require 'nokogiri'
+require 'builder'
+
+module Mantis::XSD
+
+  class ObjectRef
+
+    attr_accessor :id, :name
+
+    def initialize(id, name)
+      @id = id
+      @name = name
+    end
+
+    # Generate an ObjectRef XSD type given an #element_name
+    # that you want to tag it with.
+    #
+    # Let's say ObjectRef is id: 10, name: "public", and you want
+    # to generate the XMl for it under the <style> attribute:
+    #
+    # to_xml "style"  # should yield
+    #
+    # <status type="tns:ObjectRef">
+    #   <id>10</id>
+    #   <name>public</name>
+    # </status>
+    def to_xml(element_name)
+      builder = Nokogiri::XML::Builder.new { |xml|
+        xml.send(element_name, type: "tns:ObjectRef") do 
+          xml.id_ @id
+          xml.name @name
+        end
+      }
+      builder.doc.root
+    end
+
+
+
+  end
+end
+
+
