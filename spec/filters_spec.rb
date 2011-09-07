aa53b9f4e11c64a32b52b14077c92c085797d617
Nick Klauer (a03182) <nicholasklauer@alliantenergy.com>
2011-09-06 22:04:42 -0500

    tweak filters spec

--- spec/filters_spec.rb
+++ spec/filters_spec.rb
@@ -9,30 +9,24 @@ describe Mantis::Filters do
   end
 
   describe "listing filters" do
-
     it "should list filters for a user" do
-      skip
+      prjs = @session.projects.list
+      filters = @session.filters.list(prjs[0].id)
+      if filters.size > 0
+        must_be_same Array, filters.class, "Filters must create an Array[]"
+      end
     end
-
-    it "should return nil if no filters can be found for a user" do
-      skip
-    end
-  end
+  end # listing filters
 
   describe "issues in a filter" do
-
     it "should return an Array[] of IssueData for a filter" do
       skip
     end
-
     it "should return nil if no issues are found in a filter" do
       skip
     end
-
     it "should return a list of issue headers for a filter" do
       skip
     end
-
-  end
-
-end
+  end # issues in filter
+end # Mantis::Filters
