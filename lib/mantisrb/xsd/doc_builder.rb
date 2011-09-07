56e18ccd2987605da86373e77e1e2cd9fdbec2ab
Nick Klauer (a03182) <nicholasklauer@alliantenergy.com>
2011-09-06 23:10:19 -0500

    stub out a document builder to simplify all XSD (xxxData) classes

---
+++ lib/mantisrb/xsd/doc_builder.rb
@@ -0,0 +1,26 @@
+module Mantis::XSD::DocBuilder
+
+  def initialize(params={})
+    params.each_key { |p|
+      instance_variable_set("@#{p}", params[p])
+    }
+  end
+
+  def document(tag_name)
+    @doc ||= to_doc(tag_name)
+  end
+
+  def to_doc(tag_name, type, &block)
+    builder = Nokogiri::XML::Builder.new { |xml|
+      xml.send(tag_name, type: type) do
+        yield
+      end
+    }
+    builder.doc
+  end # to_doc
+
+  def to_element_string(tag_name)
+    document(tag_name).root.to_s
+  end # to_element_string
+
+end
