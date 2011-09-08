3f404d80a2a300b40b7f95bc2f6753b788678c97
Nick Klauer <klauer@gmail.com>
2011-09-07 21:28:08 -0500

    use autoload for xsd types
    XSD types aren't always going to be needed, so when they are, leverage
    the autoload feature to `require` them in later.

--- lib/mantisrb.rb
+++ lib/mantisrb.rb
@@ -5,9 +5,21 @@ require 'builder'
 require 'savon'
 require 'mantisrb/version'
 require 'mantisrb/session'
-require 'mantisrb/xsd/project_data'
 require 'mantisrb/issues'
-require 'mantisrb/xsd/issue_data'
 require 'mantisrb/config'
 require 'mantisrb/projects'
 require 'mantisrb/filters'
+
+# XSD types that might not be used immediately depending on what you're doing
+module Mantis::XSD
+  autoload :AccountData,                  'mantisrb/xsd/account_data'
+  autoload :AttachmentData,               'mantisrb/xsd/attachment_data'
+  autoload :CustomFieldValueForIssueData, 'mantisrb/xsd/custom_field_value_for_issue_data'
+  autoload :FilterData,                   'mantisrb/xsd/filter_data'
+  autoload :IssueData,                    'mantisrb/xsd/issue_data'
+  autoload :IssueHeaderData,              'mantisrb/xsd/issue_header_data'
+  autoload :IssueNoteData,                'mantisrb/xsd/issue_note_data'
+  autoload :ObjectRef,                    'mantisrb/xsd/object_ref'
+  autoload :ProjectData,                  'mantisrb/xsd/project_data'
+  autoload :RelationshipData,             'mantisrb/xsd/relationship_data'
+end
